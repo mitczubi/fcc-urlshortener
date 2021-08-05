c1adccd303449af97bf610c8e6c6fe6d9daee36a
Mitchell Zubich <mitchellzubich@gmail.com>
2021-08-05 16:00:29 -0400

    Adding body parser for url encoded rather than json

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const ShortUrl = mongoose.model("ShortURL", shortURLSchema);
 
 const port = process.env.PORT || 8000;
 
-app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use(cors());
 app.use(function(req, res, next) {
     console.log(req.method + " " + req.path + " " + req.ip)
